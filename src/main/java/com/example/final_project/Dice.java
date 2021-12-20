ab04295f547c45b9a0ee374f72ee7f04fb909f7f
mohd shakir <shakir20524@iiitd.a.cin>
2021-12-21 01:08:30 +0530

    v-2.6 shakirnbefor sleep

--- src/main/java/com/example/final_project/Dice.java
+++ src/main/java/com/example/final_project/Dice.java
@@ -1,26 +1,31 @@
 package com.example.final_project;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.transform.Translate;
+import javafx.util.Duration;
 
 import java.io.File;
+import java.net.URL;
 import java.util.Random;
+import java.util.ResourceBundle;
 
-public class Dice {
+public class Dice implements Initializable {
     Random random = new Random();
 
     @FXML
     private Circle myCircle;
     @FXML
     private Pane q1;
-    private Pane q2;
-    private Pane q3;
+
 
     @FXML
     private ImageView diceImage;
@@ -37,6 +42,7 @@ public class Dice {
             public void run(){
                 System.out.println("Thread Running");
 
+
                 try {
                     for (int i = 0; i < 15; i++) {
                         File file = new File("src/main/resources" + (random.nextInt(6)+1)+".jpeg");
@@ -52,12 +58,19 @@ public class Dice {
 
         thread.start();
     }
-
-    public void trans(){
-
-        myCircle.setCenterY(-q1.getHeight());
+//61.0
+//74.0
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       TranslateTransition transition =new TranslateTransition();
+       transition.setNode(myCircle);
+       transition.setToX(0);
+       transition.setByY(-q1.getHeight());
+
+       transition.setDuration(Duration.millis(2000));
+       transition.play();
     }
 
 }
